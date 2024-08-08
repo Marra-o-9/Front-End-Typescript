0a9cd8fed37de8b17d52cb8bb6292233ddfcad47
Marra <marrabarbosa9@gmail.com>
2024-08-08 09:35:53 -0300

    Primeira interação com Typescript

---
+++ saudacao.js
@@ -0,0 +1,5 @@
+function saudacao(nome) {
+    console.log("Ol\u00E1, meu nome \u00E9 ".concat(nome, "!"));
+}
+var nome = "Marra";
+saudacao(nome);
---
+++ saudacao.ts
@@ -0,0 +1,7 @@
+function saudacao(nome: string) {
+    console.log(`Olá, meu nome é ${nome}!`);
+}
+
+let nome: string = "Marra";
+
+saudacao(nome)
